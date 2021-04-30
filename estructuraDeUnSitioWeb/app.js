1a61a03efb6276ccf6e7ed43f832ad065d08f327
catriel10 <catriel10@gmail.com>
2021-04-30 19:08:31 -0300

    1 estructura web ahora funciona

--- estructuraDeUnSitioWeb/app.js
+++ estructuraDeUnSitioWeb/app.js
@@ -11,33 +11,3 @@ console.log ("Mi servidor express esta funcionando en port 3030")
 app.get('/', (req, res) => {
     res.sendFile (path.join (__dirname, "/views/src/home.html"))
 });
-
-
-app.get('/babbage', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/babbage/babbage.html"))
-});
-
-app.get('/berners-lee', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/berners-lee.html"))
-});
-
-app.get('/clarke', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/clarke.html"))
-});
-
-app.get('/hamilton', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/hamilton.html"))
-});
-
-app.get('/hopper', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/hopper.html"))
-});
-
-app.get('/lovelace', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/lovelace.html"))
-});
-
-app.get('/turing', (req, res) => {
-    res.sendFile (path.join (__dirname, "/views/turing.html"))
-});
-
