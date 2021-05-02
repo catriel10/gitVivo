83237c25d62d4cb32cbb94fb14f4e903decd6be6
catriel10 <catriel10@gmail.com>
2021-05-01 21:19:20 -0300

    Nace mercadoLiebre

---
+++ mercadoLiebre/app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+
+app.use(express.static("public"))
+
+app.listen (3030, ()=>{
+console.log ("Mi servidor express esta funcionando en port 3030")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile (path.join (__dirname, "/views/home.html"))
+});
