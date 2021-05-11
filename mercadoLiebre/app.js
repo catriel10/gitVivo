009f30ce8834f1d9281a437cdf0b2201b6ec45d0
catriel10 <catriel10@gmail.com>
2021-05-11 14:58:26 -0300

    Clase 14

--- mercadoLiebre/app.js
+++ mercadoLiebre/app.js
@@ -1,8 +1,8 @@
 const express = require ("express");
 const app = express ();
 const path = require ("path");
-const publicPatch = path.resolve (__dirname,'./public')
-app.use(express.static(publicPatch))
+
+app.use( express.static(path.resolve (__dirname,"./public")));
 
 app.listen (3030, ()=>{
 console.log ("Mi servidor express esta funcionando en port 3030")
